da60ab62913c9a33105f8b934dcd9a74eb3a08e2
rasedul99 <rasedul2199@gmail.com>
2022-11-10 22:19:47 +0600

    service added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,16 +5,20 @@ import pizza from "../assets/images/category-02.png";
 import asianfood from "../assets/images/category-03.png";
 import rowmeat from "../assets/images/category-04.png";
 import hero from "../assets/images/hero.png";
+import service1 from "../assets/images/service-01.png";
+import service2 from "../assets/images/service-02.png";
+import service3 from "../assets/images/service-03.png";
 import Category from "../components/FoodsCategory/Category";
 
 const Home = () => {
   return (
     <div>
+      {/* hero section */}
       <div className=" grid grid-cols-1 md:grid-cols-2  justify-items-center items-center">
         <div className="md:-mt-10 mt-10  flex flex-col justify-center items-center md:inline">
           <p className="text-xl font-medium mb-4 ">Easy way to make an order</p>
           <p className="text-4xl mb-6 w-3/4">
-            <span className="text-red-600 5">HUNGRY?</span> Just wait food at{" "}
+            <span className="text-red-600 ">HUNGRY?</span> Just wait food at{" "}
             <span className="text-red-600">your door</span>
           </p>
           <p className="text-base my-6 font-['Istok_Web'] mx-3  md:mx-0">
@@ -52,11 +56,63 @@ const Home = () => {
           <img src={hero} alt="" />
         </div>
       </div>
+      {/* Food Category */}
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-        <Category  name="Fastfood" image={fastfood} />
+        <Category name="Fastfood" image={fastfood} />
         <Category name="Pizza" image={pizza} />
         <Category name="Asian Food" image={asianfood} />
         <Category name="Row Meats" image={rowmeat} />
+      </div>
+
+      {/* section three */}
+      <div>
+        <div className="flex flex-col items-center justify-center py-20">
+          <h4 className="text-red-600  text-xl font-medium mb-5">
+            What we serve
+          </h4>
+          <h3 className="text-4xl mb-5 text-center">
+            Just sit back at home <br></br> we will{" "}
+            <span className="text-red-600 ">take care</span>{" "}
+          </h3>
+          <div className="text-base font-['Istok_Web'] ">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+              officiis?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+              eius.
+            </p>
+          </div>
+        </div>
+        <div className=" md:flex ">
+          <div className="flex flex-col gap-y-4 my-5 items-center ">
+            <img src={service1} className="w-28" />
+            <h4 className="text-xl font-medium">Quick Delivery</h4>
+            <p className="text-base  font-['Istok_Web'] text-center">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus,
+              doloremque.
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-y-4 my-5 items-center ">
+            <img src={service2} className="w-28" />
+            <h4 className="text-xl font-medium">Super Dine In</h4>
+            <p className="text-base  font-['Istok_Web'] text-center">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus,
+              doloremque.
+            </p>
+          </div>
+          <div className="flex flex-col gap-y-4 my-5 items-center  ">
+            <img src={service3} className="w-28" />
+            <h4 className="text-xl font-medium">Easy Pick Up</h4>
+            <p className="text-base  font-['Istok_Web'] text-center">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus,
+              doloremque.
+            </p>
+          </div>
+        </div>
+        
       </div>
     </div>
   );
